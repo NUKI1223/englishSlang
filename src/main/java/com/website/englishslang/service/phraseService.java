7f20fb9bfd26e186cd4b1a96e5b4b9542039c6d7
Vlad Karpov <mrdelron966@gmail.com>
2024-05-10 16:40:18 +0600

    create a loadPhrases method

--- src/main/java/com/website/englishslang/service/phraseService.java
+++ src/main/java/com/website/englishslang/service/phraseService.java
@@ -1,7 +1,28 @@
 package com.website.englishslang.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class phraseService {
+
+    private Map<String, String> dictionary;
+    public phraseService(){
+        dictionary = new HashMap<>();
+        loadPhrases();
+    }
+    public void loadPhrases(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        try{
+            Map<String, String> phrases = objectMapper.readValue(getClass().getResourceAsStream("/phrases.json"), Map.class);
+            dictionary.putAll(phrases);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
