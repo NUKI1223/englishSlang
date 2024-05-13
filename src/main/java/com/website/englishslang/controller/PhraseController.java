e230706f1f90eaabfbcd7435fb6e5c0776e957d6
Vlad Karpov <mrdelron966@gmail.com>
2024-05-13 19:07:18 +0600

    create a full site

--- src/main/java/com/website/englishslang/controller/PhraseController.java
+++ src/main/java/com/website/englishslang/controller/PhraseController.java
@@ -2,36 +2,44 @@ package com.website.englishslang.controller;
 
 import com.website.englishslang.service.PhraseService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@RestController
+@Controller
 public class PhraseController {
-    private PhraseService phraseService;
+    private final PhraseService phraseService;
     public PhraseController(){
         phraseService = new PhraseService();
     }
     @GetMapping("/")
-    public String showMainPage(){
-
-        return phraseService.getDictionary().toString();
+    public String showMainPage(Model model){
+        Map<String, String> dict = phraseService.getDictionary();
+        model.addAttribute("words", dict.keySet());
+        model.addAttribute("definitions", dict.values());
+        return "index";
     }
     @GetMapping("/phrase/{word}")
-    public String getDefinition(@PathVariable String word){
-        return phraseService.getDefiniton(word);
+    public String getDefinition(@PathVariable String word, Model model){
+        String definiton = phraseService.getDefiniton(word);
+        model.addAttribute("word", word);
+        model.addAttribute("definition", definiton);
+        return "phrase";
 
     }
     @GetMapping("/search")
-    public String searchDefinition(@RequestParam String query){
+    public String searchDefinition(@RequestParam String query, Model model){
         if(phraseService.getDictionary().containsKey(query)){
-            return getDefinition(query);
+            String results = phraseService.getDefiniton(query);
+            model.addAttribute("results", results);
+            model.addAttribute("word", query);
+            return "search";
         }
         else {
-            return "Такого слова не найдено!";
+            return "search";
         }
     }
 
